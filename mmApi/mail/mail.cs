622f2b7b0cf667edf6bd1ddaadfdf1e47fca64fa
HuXioAn <hu7006@outlook.com>
2024-02-27 11:37:06 +0100

    add mail structure

---
+++ mmApi/mail/mail.cs
@@ -0,0 +1,30 @@
+using System;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace mmApi.Mail;
+
+public static class mail{
+
+    static mail(){
+        //initiating the resources, only once
+
+
+    }
+
+    static public MimeMessage? composeMail(){
+
+
+        return null;
+    }
+
+    static public int sendMail(MimeMessage mail){
+
+        return 0;
+    }
+
+
+
+}
